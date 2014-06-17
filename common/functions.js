d5e3523dbc6d2df427619f2961fc4d5363a8b636
Dimitar <dmihaylov93@gmail.com>
2014-06-17 12:28:10 +0300

    Creating the base

---
+++ common/functions.js
@@ -0,0 +1,18 @@
+"use strict";
+$(function(){
+
+    windowRefresh();
+	$(window).on('resize',function(){
+		windowRefresh();
+	});
+
+	
+    
+
+
+
+});
+
+function windowRefresh(){
+	// functionality on window refresh
+}
