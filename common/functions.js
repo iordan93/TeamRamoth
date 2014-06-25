152e023e6ba41e21d46f15bafc6afa2e75b9cf82
Dimitar <dmihaylov93@gmail.com>
2014-06-25 22:50:32 +0300

    Tabs

--- common/functions.js
+++ common/functions.js
@@ -2,6 +2,7 @@
 $(function(){
 
     windowRefresh();
+
 	$(window).on('resize',function(){
 		windowRefresh();
 	});
@@ -15,4 +16,15 @@ $(function(){
 
 function windowRefresh(){
 	// functionality on window refresh
+
+	// fix tabs text
+	$("ul.tabs li").each(function(){
+		var anchor=$(this).find("a");
+		var span=anchor.find("span");
+
+		span.css({
+			'top':(anchor.outerHeight()-span.outerHeight())/2,
+			'left':(anchor.outerWidth()-span.outerWidth())/2,
+		});
+	});
 }
