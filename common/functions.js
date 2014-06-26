824547c31631c94bb53d222cfe7d7d0454715102
Dimitar <dmihaylov93@gmail.com>
2014-06-26 11:45:43 +0300

    Grid View List Vie

--- common/functions.js
+++ common/functions.js
@@ -7,6 +7,17 @@ $(function(){
 		windowRefresh();
 	});
 
+	$(".view-choice a").click(function(e){
+
+		$(".tabs-content ul").removeClass("list-view");
+		$(".tabs-content ul").removeClass("grid-view");
+
+		$(".tabs-content ul").addClass($(this).attr("class")+"-view");
+
+		centerImages();
+
+		e.preventDefault();
+	});
 	
     
 
@@ -26,5 +37,41 @@ function windowRefresh(){
 			'top':(anchor.outerHeight()-span.outerHeight())/2,
 			'left':(anchor.outerWidth()-span.outerWidth())/2,
 		});
+	});
+
+	
+	centerImages();
+}
+
+function centerImages(){
+	// center grid images
+	$("ul.grid-view li").each(function(){
+
+		var anchor=$(this).find("a");
+		var img = anchor.find("img");
+		anchor.css({
+			'height':200
+		});
+
+		img.css({
+			'top':(anchor.outerHeight()-img.outerHeight())*0.5,
+			'left':(anchor.outerWidth()-img.outerWidth())*0.5
+		});
+
+	});
+
+	$("ul.list-view li").each(function(){
+
+		var anchor=$(this).find("a");
+		var img = anchor.find("img");
+		anchor.css({
+			'height':img.outerHeight()
+		});
+
+		img.css({
+			'top':0,
+			'left':0
+		});
+
 	});
 }
