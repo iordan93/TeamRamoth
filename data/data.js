09ade471134809e0b5e195ada5c6c783b05aa542
Yordan Darakchiev <iordan93@gmail.com>
2014-07-02 15:36:12 +0300

    Added database

--- data/data.js
+++ data/data.js
@@ -1,50 +1,130 @@
 ﻿[
+    {
+        "title": "Just GitHub",
+        "description": "When someone of your friends commits before you",
+        "category": "when-you",
+        "imageUrl": "when-someone-commits-before-you.gif",
+        "likes": "18",
+        "dislikes": "122"
+    },
 	{
-		"title": "Precious memories",
-		"description": "Svetlio, as many of his friends know him, is a famous party star",
-		"category": "memes",
-		"imageUrl": "LOTR.png",
-		"likes": "18",
-		"dislikes": "122"
+	    "title": "Precious memories",
+	    "description": "Svetlio, as many of his friends know him, is a famous party star",
+	    "category": "memes",
+	    "imageUrl": "LOTR.png",
+	    "likes": "180",
+	    "dislikes": "2"
+	},
+    {
+        "title": "Recursion",
+        "description": "To understand recursion, you have to understand recursion first",
+        "category": "it-life",
+        "imageUrl": "recursion.gif",
+        "likes": "888",
+        "dislikes": "12"
+    },
+	{
+	    "title": "Miss SoftUni",
+	    "description": "Vlado is on his way to becoming a ninja... or Miss SoftUni (Not for the faint-hearted!)",
+	    "category": "when-you",
+	    "imageUrl": "dartvader.png",
+	    "likes": "50",
+	    "dislikes": "47"
 	},
 	{
-		"title": "Miss SoftUni",
-		"description": "Miss SoftUni",
-		"category": "when-you",
-		"imageUrl": "dartvader.png",
-		"likes": "50",
-		"dislikes": "47"
+	    "title": "Health before care",
+	    "description": "... Or some thoughts about medicinal plants :)",
+	    "category": "memes",
+	    "imageUrl": "gandalf.png",
+	    "likes": "156",
+	    "dislikes": "20"
 	},
 	{
-		"title": "Health before care",
-		"description": "Health before care",
-		"category": "memes",
-		"imageUrl": "gandalf.png",
-		"likes": "156",
-		"dislikes": "20"
+	    "title": "Brace yourselves",
+	    "description": "The exams are coming!",
+	    "category": "when-you",
+	    "imageUrl": "exams-are-coming.jpg",
+	    "likes": "56",
+	    "dislikes": "0"
+	},
+    {
+        "title": "That feeling",
+        "description": "... when you read the exam tasks for the first time",
+        "category": "when-you",
+        "imageUrl": "exam-feelings.gif",
+        "likes": "56",
+        "dislikes": "0"
+    },
+	{
+	    "title": "When you have no errors",
+	    "description": "Dat feeling!",
+	    "category": "it-life",
+	    "imageUrl": "my-code-compiled.jpg",
+	    "likes": "133",
+	    "dislikes": "25"
 	},
 	{
-		"title": "Exam",
-		"description": "Exam is coming",
-		"category": "when-you",
-		"imageUrl": "matrix.png",
-		"likes": "56",
-		"dislikes": "0"
+	    "title": "After Exam",
+	    "description": "Nakov said: Задачките са много лесни...",
+	    "category": "when-you",
+	    "imageUrl": "matrix.png",
+	    "likes": "123",
+	    "dislikes": "45"
 	},
-	{
-		"title": "When you have no errors",
-		"description": "",
-		"category": "it-life",
-		"imageUrl": "my-code-compiled.jpg",
-		"likes": "133",
-		"dislikes": "25"
-	},
-	{
-		"title": "After Exam",
-		"description": "Nakov said: Задачките са много лесни...",
-		"category": "when-you",
-		"imageUrl": "matrix.png",
-		"likes": "123",
-		"dislikes": "45"
-	}
+    {
+        "title": "The most sweared at browser?",
+        "description": "Nakov? I can't hear you!",
+        "category": "it-life",
+        "imageUrl": "baivlado1.png",
+        "likes": "23",
+        "dislikes": "0"
+    },
+    {
+        "title": "Miss SoftUni: The Final",
+        "description": "The competition was really tough!",
+        "category": "memes",
+        "imageUrl": "harrypottrer.png",
+        "likes": "52",
+        "dislikes": "1"
+    },
+    {
+        "title": "Miss SoftUni: The Final",
+        "description": "The competition was really tough!",
+        "category": "when-you",
+        "imageUrl": "harrypottrer.png",
+        "likes": "52",
+        "dislikes": "1"
+    },
+    {
+        "title": "SoftUni vs Matrix",
+        "description": "No one could ever dream about this!",
+        "category": "memes",
+        "imageUrl": "matrix2.png",
+        "likes": "2",
+        "dislikes": "100"
+    },
+    {
+        "title": "HTML Teamwork: The reality",
+        "description": "Private area! Sorry, no servers allowed here!",
+        "category": "memes",
+        "imageUrl": "no-server-side.jpg",
+        "likes": "200",
+        "dislikes": "1"
+    },
+    {
+        "title": "OOP is for pussies",
+        "description": "And don't even talk about Haskell or Lisp!",
+        "category": "memes",
+        "imageUrl": "real-programmers.jpg",
+        "likes": "252",
+        "dislikes": "0"
+    },
+    {
+        "title": "Beer",
+        "description": "The most essential body fluid of a programmer!",
+        "category": "it-life",
+        "imageUrl": "sparta.png",
+        "likes": "26",
+        "dislikes": "42"
+    }
 ]
